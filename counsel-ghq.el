e4a96e9a120098da6d069acc8cdc519d0e56a675
Keisuke Noguchi <windymelt@3qe.us>
2017-06-29 15:55:42 +0900

    Followed lint

--- counsel-ghq.el
+++ counsel-ghq.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/windymelt/counsel-ghq
 ;; Keywords: lisp counsel ghq
 ;; Version: 0.0.1
-;; Package-Requires: (ivy)
+;; Package-Requires: ((emacs "24") (ivy "0.9.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
