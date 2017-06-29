41cbf1126f85848ca31c9e1f8a35cba232edc7fd
Keisuke Noguchi <windymelt@3qe.us>
2017-06-29 13:30:53 +0900

    Wrote skelton

---
+++ counsel-ghq.el
@@ -0,0 +1,32 @@
+;;; counsel-ghq.el --- ghq with counsel interface -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016 by Keisuke Noguchi
+
+;; Author: Keisuke Noguchi <windymelt@3qe.us>
+;; URL: https://github.com/windymelt/counsel-ghq
+;; Keywords: lisp counsel ghq
+;; Version: 0.0.1
+;; Package-Requires: (()) TODO
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; counsel-ghq.el provides a counsel interface to "ghq".
+
+;;; Code:
+
+(provide 'counsel-ghq)
+
+;;; counsel-ghq.el ends here
